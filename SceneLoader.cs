2bc1cfa552b1b2301e30921b29cde8779be8ca3c
YaroslavHaistruk <gaystruko1@gmail.com>
2021-09-30 11:11:04 +0200

    Create SceneLoader.cs

---
+++ SceneLoader.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneLoader : MonoBehaviour
+{
+	public int scene;
+	
+    public void SceneLoad(int sn)
+    {
+        SceneManager.LoadScene(sn);
+    }
+	public void Exit()
+    {
+        Application.Quit();
+    }
+	void OnTriggerEnter2D(Collider2D cl)
+    {
+		if (cl.gameObject.tag == "paczka")
+        {
+			SceneManager.LoadScene(scene);
+        }
+		if (cl.gameObject.tag == "paczkaBlue")
+        {
+			Application.Quit();
+        }
+		
+    }
+}
